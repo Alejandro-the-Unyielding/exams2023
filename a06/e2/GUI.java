18ef81c8d4ff29ec33cc97b34a1942cbacd4a45d
Daniel Alejandro <d.alejandrohorna@gmail.com>
2025-02-06 20:56:53 +0100

    e2 not done

--- a06/e2/GUI.java
+++ a06/e2/GUI.java
@@ -2,16 +2,18 @@ package e2;
 
 import javax.swing.*;
 import java.util.*;
-import java.util.List;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class GUI extends JFrame {
     
     private static final long serialVersionUID = -6218820567019985015L;
-    private final List<JButton> cells = new ArrayList<>();
+    private final Map<JButton, Position> cells = new HashMap<>();
+    //private final Logic logic;
     
     public GUI(int width) {
+
+        //this.logic = new LogicImpl(width);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(70*width, 70*width);
         
@@ -19,20 +21,25 @@ public class GUI extends JFrame {
         this.getContentPane().add(panel);
         
         ActionListener al = e -> {
+            @SuppressWarnings("unused")
             var jb = (JButton)e.getSource();
-        	jb.setText(String.valueOf(cells.indexOf(jb)));
+            
+        	
         };
                 
         for (int i=0; i<width; i++){
             for (int j=0; j<width; j++){
-            	var pos = new Pair<>(j,i);
-                final JButton jb = new JButton(pos.toString());
-                this.cells.add(jb);
+                final JButton jb = new JButton();
+                this.cells.put(jb, new Position(i, j));
                 jb.addActionListener(al);
                 panel.add(jb);
             }
         }
         this.setVisible(true);
     }
+
+
+
+
     
 }
